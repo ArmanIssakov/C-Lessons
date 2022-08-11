723b626364710565e1d2efb87bb97bb61d5abba5
Armanissakov <arman.isakov.96@mail.ru>
2022-08-11 14:26:31 +0600

    task17 and task18 is done

---
+++ Task17/Program.cs
@@ -0,0 +1,35 @@
+﻿// 17. Напишите программу, которая принимает на вход
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+// номер четверти плоскости, в которой находится эта
+// точка.
+
+Console.WriteLine("Введите координаты точки:");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+string WhereI(int numX, int numY)
+{
+ if(numX > 0 && numY > 0) return "Мы находимся в первой четверти";
+
+ if (numX < 0 && numY > 0) return   "Мы находимся во второй четверти";
+
+ if (numX < 0 && numY < 0) return  "Мы находимся во третьей четверти";
+
+ if (numX > 0 && numY < 0) return "Мы находимся во четвертой четверти";
+ 
+ return "Введены некорректные координаты";
+}
+
+
+
+string result = WhereI(x,y);
+Console.WriteLine($"{result}");
+
+// string Quarter (int xc, int yc)
+// {
+
+// }
+
+
---
+++ Task18/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите номер четверти:");
+int x = Convert.ToInt32(Console.ReadLine());
+string QuarterNum(int numX)
+{
+    if (numX == 1) return "Диапозон х > 0 и y > 0";
+    if (numX == 2) return "Диапозон х < 0 и y > 0";
+    if (numX == 3) return "Диапозон х < 0 и y < 0";
+    if (numX == 4) return "Диапозон х > 0 и y < 0";
+    return "Введены некорректные координаты";
+}
+string result = QuarterNum(x);
+Console.WriteLine($"{result}");
+
+
