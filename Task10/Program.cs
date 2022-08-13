047cb7fb08c2629ae25000dd61aca3250d121aaf
Armanissakov <arman.isakov.96@mail.ru>
2022-08-13 20:16:03 +0600

    upgrate task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -5,7 +5,7 @@
 // 918 -> 1
 Console.Write("Введите положительное трехзначное число, для вывода второй цифры этого числа:");
 int number = Convert.ToInt32(Console.ReadLine());
-int result = number % 100;
+int result = 0;
 if (number < 1000 && number > 99)
 {
     result = number % 100;
