afe690ec3d9cc3d95761a249ae3e6c2468633d93
Armanissakov <arman.isakov.96@mail.ru>
2022-08-31 22:24:19 +0600

    task47 is done

---
+++ Task47/Program.cs
@@ -0,0 +1,30 @@
+﻿double[,] CreateMatrixRndDouble(int row, int col)
+{
+    double[,] matrix = new double[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            matrix[i, j] = rnd.NextDouble();
+            matrix[i, j] = matrix[i, j] * 10;
+            matrix[i, j] = Math.Round(matrix[i, j], 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, 7");
+            else Console.Write($"{matrix[i, j]}, 3");
+        }
+        Console.WriteLine("]");
+    }
+}
+double[,] matr = CreateMatrixRndDouble(4, 4);
+PrintMatrix(matr);
